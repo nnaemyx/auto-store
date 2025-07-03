00098e6388f2676a31085f25f9794a452f076525
johnpaul <edehjohnpaul@gmail.com>
2025-07-03 19:21:15 +0100

    image optimization removed

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
     domains: ['admin.autostoreng.com', 'res.cloudinary.com', 'admin.autostores.ng'],
-    
+    unoptimized: true,
   },
   output: 'standalone',
 };
